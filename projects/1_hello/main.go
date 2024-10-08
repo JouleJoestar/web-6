f5b8f7ab9ab2eb3e07114d77da080eb760f151ec
Timur <timurtein@gmail.com>
2024-10-08 11:23:05 +0300

    lab6

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,22 @@
 package main
 
-// здесь надо написать код
+// некоторые импорты нужны для проверки
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	// и здесь тоже
+	http.HandleFunc("/getMessage", handleGetRequest)
+	fmt.Println("Server started on :8080")
+	http.ListenAndServe(":8080", nil)
 }
+
+func handleGetRequest(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, web!")
+}
+
+/*- Create (POST)
+- Read (GET)
+- Update (PUT/PATCH)
+- Delete (DELETE)*/
